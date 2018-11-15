05c7e3b4e7b8eb6981440460f6bf8adc9f288048
unknown <brettbrun@gmail.com>
2018-11-14 23:41:36 -0500

    continuing to build console app. TODO: handle invalid format errors.

--- BabySitter.App/Program.cs
+++ BabySitter.App/Program.cs
@@ -13,30 +13,34 @@ namespace BabySitter.App
             BabySitterService service = new BabySitterService();
             Console.WriteLine(INTRODUCTION);
 
-            string familyId = "";
-            string startTime = "";
-            string endTime = "";
+            string familyIdInput = "";
+            string startTimeInput = "";
+            string endTimeInput = "";
+            TimeSpan startTime = new TimeSpan();
+            TimeSpan endTime = new TimeSpan();
 
             do 
             {
                 Console.Write("What family did you work for tonight? (A, B, or C): ");
-                familyId = Console.ReadLine();
+                familyIdInput = Console.ReadLine();
                 
-            } while (!service.IsValidFamily(familyId));
+            } while (!service.IsValidFamily(familyIdInput));
 
             do 
             {
-                Console.WriteLine("When did you start work? (Accepted formats: '12:00', '02:00', '17:00' '01:00PM', '10:00 AM'): ");
-                startTime = Console.ReadLine();
+                Console.WriteLine("When did you start work? (No earlier than 5:00 PM) (Accepted formats: '17:00', '05:00PM', '10:00 AM'): ");
+                startTimeInput = Console.ReadLine();
+                startTime = service.GetTimeSpanFromString(startTimeInput);
                 
-            } while (!service.IsInputTimeFormatValid(startTime));
+            } while (!service.IsInputTimeFormatValid(startTimeInput) || !service.IsStartTimeValid(startTime));
 
             do 
             {
-                Console.WriteLine("When did you leave work? (Accepted formats: '12:00', '02:00', '17:00' '01:00PM', '10:00 AM'): ");
-                endTime = Console.ReadLine();
+                Console.WriteLine("When did you leave work? (No later than 4:00 AM) (Accepted formats: '17:00', '05:00PM', '10:00 AM'): ");
+                endTimeInput = Console.ReadLine();
+                endTime = service.GetTimeSpanFromString(endTimeInput);
                 
-            } while (!service.IsInputTimeFormatValid(endTime));
+            } while (!service.IsInputTimeFormatValid(endTimeInput) || !service.IsEndTimeValid(endTime) || !service.IsEndTimeAfterStartTime(startTime, endTime));
         }
     }
 }
